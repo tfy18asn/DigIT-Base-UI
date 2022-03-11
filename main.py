85655836714c8e3338fdc2459e1118e6d4e0b999
--- <--->
2022-03-11 09:59:36 +0100

    template recompilation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-print('Base.')
-
-
+import os
 import flask, jinja2
 print('Flask:', flask.__version__)
 print('Jinja:', jinja2.__version__)
@@ -8,16 +6,29 @@ print('Jinja:', jinja2.__version__)
 
 
 
-def create_app():
-    app = flask.Flask(__name__)
-    print('Instance path:', app.instance_path)
+def create_app(template_folders=['templates']):
+    app = flask.Flask(__name__, root_path='./')
+    print('Root path:       ', app.root_path)
+    print('Template folder: ', app.template_folder)
+    print('Static folder:   ', app.static_folder)
+    print()
 
+    recompile_templates(template_folders)
 
     @app.route('/')
     def index():
-        return "Hi, I'm Base."
+        recompile_templates(template_folders)
+        return app.send_static_file('index.html')
 
     return app
+
+
+def recompile_templates(template_folders=['templates'], static_folder='static'):
+    env   = jinja2.Environment(loader=jinja2.FileSystemLoader(template_folders))
+    tmpl  = env.get_template('index.html')
+    outf  = os.path.join(static_folder, 'index.html')
+    os.makedirs(os.path.dirname(outf), exist_ok=True)
+    open(outf,'w').write(tmpl.render())
 
 
 if __name__ == '__main__':
