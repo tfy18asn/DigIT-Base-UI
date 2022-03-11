e461235feda61181705aa4091ffe146b13988301
--- <--->
2022-03-11 07:36:29 +0100

    basic flask

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 print('Base.')
+
+
+import flask, jinja2
+print('Flask:', flask.__version__)
+print('Jinja:', jinja2.__version__)
+
+
+
+
+def create_app():
+    app = flask.Flask(__name__)
+    print('Instance path:', app.instance_path)
+
+
+    @app.route('/')
+    def index():
+        return "Hi, I'm Base."
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app().run(port=5001)
